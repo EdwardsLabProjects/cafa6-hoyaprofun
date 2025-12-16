d7d025323bea6c05d00091fc0d7852bf8b520c48
Nathan Edwards <edwardsnj@gmail.com>
2025-12-16 09:56:37 -0500

    more plots

--- plots.py
+++ plots.py
@@ -11,17 +11,14 @@ result_file = CONFIG["RESULT"]
 model_pred_file = CONFIG["MODEL_RESULT"]
 
 if CONFIG['MERGE_WITH_GOA']:
-    steps.write_submission_plot(CONFIG,model_pred_file,result_file,outfile="confidence.png")
-
-    train_truth = steps.load_train_terms_ground_truth(CONFIG)
-    steps.write_precall_plot(CONFIG,train_truth,model_pred_file,result_file,outfile="train_pr.png")
-
+    result_files = [ model_pred_file, result_file ]
 else:
-    steps.write_submission_plot(CONFIG,result_file,outfile="confidence.png")
+    results_files = [ result_file ]
 
-    train_truth = steps.load_train_terms_ground_truth(CONFIG)
-    steps.write_precall_plots(CONFIG,train_truth,result_file,outfile="train_pr.png")
+steps.write_submission_plot(CONFIG,*result_files,outfile="confidence.png")
 
+train_truth = steps.load_train_terms_ground_truth(CONFIG)
+steps.write_precall_plot(CONFIG,train_truth,*result_files,outfile="train_pr.png")
 
 
 
