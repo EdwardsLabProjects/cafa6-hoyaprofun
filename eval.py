bcbff6a0b2dba952fdd417edfd31ca74d8ccc0c6
Nathan Edwards <edwardsnj@gmail.com>
2025-12-16 20:24:28 -0500

    implement cafa plots

--- eval.py
+++ eval.py
@@ -3,16 +3,12 @@
 import sys
 import steps
 
-print("\n[0/6] Load configuration and initial files...")
-
 CONFIG = steps.configuration(sys.argv[1:])
 
 result_file = CONFIG["RESULT"]
 model_pred_file = CONFIG["MODEL_RESULT"]
 
-steps.run_cafa6_eval(CONFIG,model_pred_file)
-steps.run_cafa6_eval(CONFIG,result_file)
-
+steps.run_cafa6_eval(CONFIG,model_pred_file,result_file)
 
 
 
