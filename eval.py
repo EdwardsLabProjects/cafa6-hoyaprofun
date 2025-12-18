868f36b067aaa083b3c77d0f05d9fa5d8861738f
Nathan Edwards <edwardsnj@gmail.com>
2025-12-18 15:05:23 -0500

    logging

--- eval.py
+++ eval.py
@@ -5,8 +5,8 @@ import steps
 
 CONFIG = steps.configuration(sys.argv[1:])
 
-result_file = CONFIG["RESULT"]
-model_pred_file = CONFIG["MODEL_RESULT"]
+result_file = getfile(CONFIG["RESULT"])
+model_pred_file = getfile(CONFIG["MODEL_RESULT"])
 
 steps.run_cafa6_eval(CONFIG,model_pred_file,result_file)
 steps.cafa6_plots(CONFIG)
