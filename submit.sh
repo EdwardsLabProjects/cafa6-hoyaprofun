9c7ebbe854f434cdd425c4d6e3984c4533674630
Nathan Edwards <edwardsnj@gmail.com>
2026-01-23 11:59:38 -0500

    update

--- submit.sh
+++ submit.sh
@@ -3,4 +3,7 @@ if [ "$1" = "" -o "$2" = "" ]; then
   echo "Usage: submit.sh submission.tsv \"message\"" 1>&2
   exit 1
 fi
-./kaggle.sh competitions submit -c cafa-6-protein-function-prediction -f "$1" -m "$2"
+TMP_DIR=$(mktemp -d) || exit 1
+cp "$1" "$TMP_DIR"/submission.tsv
+trap 'rm -rf "$TMP_DIR"' EXIT
+./kaggle.sh competitions submit -c cafa-6-protein-function-prediction -f "$TMP_DIR/submission.tsv" -m "$2"
